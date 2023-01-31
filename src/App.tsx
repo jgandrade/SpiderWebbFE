82066cf794eb1b92479bc986670d0dc59c207a54
jgandrade1 <johnglenn.andrade@gmail.com>
2023-02-01 04:25:32 +0800

    Connected to Backend for Registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,67 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import firebaseInit from "./firebaseConfig";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 
 function App() {
-  useEffect(() => {
-    const data = axios
-      .get("http://localhost:5000")
-      .then((data) => console.log(data))
-      .catch((err) => err);
-  }, []);
+  type userType = {
+    name: string | null;
+    image: string | null;
+  };
 
-  return <div className="App">Hello World!</div>;
+  const [result, setResult] = useState<any>();
+  const [user, setUser] = useState<userType>({
+    name: "",
+    image: "",
+  });
+
+  const provider = new GoogleAuthProvider();
+  const app = firebaseInit();
+  const auth = getAuth(app);
+
+  function register() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        axios
+          .post("http://localhost:5000/register", {
+            uid: result.user.uid,
+            name: result.user.displayName,
+            email: result.user.email,
+          })
+          .then((res) => {
+            if (res.data.response === true) alert("Registered");
+            else alert("Account already exist");
+          })
+          .catch((err) => alert(err));
+
+        setUser({
+          name: result.user.displayName,
+          image: result.user.photoURL,
+        });
+        setResult(result);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  }
+
+  return (
+    <div className="App">
+      {user.name ? (
+        <div>
+          <img src={`${user.image}`} alt="profile-pic" />
+          <p>{user.name}</p>
+          <p>Details:</p>
+          <p>{JSON.stringify(result.user)}</p>
+        </div>
+      ) : (
+        <button onClick={register}>SIGN-IN</button>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta?.env.VITE_APP_apiKey,
+  authDomain: import.meta.env.VITE_APP_authDomain,
+  projectId: import.meta.env.VITE_APP_projectId,
+  storageBucket: import.meta.env.VITE_APP_storageBucket,
+  messagingSenderId: import.meta.env.VITE_APP_messagingSenderId,
+  appId: import.meta.env.VITE_APP_appId,
+  measurementId: import.meta.env.VITE_APP_measurementId,
+};
+
+// Initialize Firebase
+export default function firebaseInit() {
+  return initializeApp(firebaseConfig);
+}
